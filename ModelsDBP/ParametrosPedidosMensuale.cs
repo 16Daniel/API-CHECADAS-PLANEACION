49c291cf39d0e09f1e1f17fa047e0e8a85841e7e
Arturo-Mejia-dev <arturo.m@operamx.com>
2026-08-13 16:08:54 -0600

    planeacion mensual

---
+++ ModelsDBP/ParametrosPedidosMensuale.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace API_PEDIDOS.ModelsDBP
+{
+    public partial class ParametrosPedidosMensuale
+    {
+        public int Id { get; set; }
+        public int TiempoDeEntrega { get; set; }
+        public int PeriodoDeRevision { get; set; }
+        public double NivelDeServicio { get; set; }
+        public int MesesConDatos { get; set; }
+        public string? DataDivisionPedidos { get; set; }
+    }
+}
